f429bbf4c5ad02f60f974a9c97b86467b76183ba
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2023-08-28 01:18:28 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,13 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Libraries */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdbool.h>
 #include <sys/wait.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 /* Structures */
 typedef struct environmentVariable {
@@ -21,20 +21,22 @@ typedef struct environment {
 } env;
 
 /* Function declarations */
+char *getenve(const char *name);
+int setenve(const char *name, const char *value);
+int unsetenve(const char *name);
 void make_environment(env *environment, char *env[]);
-char* _getenv(char *name, env *environment);
+char *_getenv(char *name, env *environment);
 int _setenv(char *name, char *value, env *environment);
 int _unsetenv(char *name, env *environment);
 void inputError(int n_char, char *buffer, char **array);
 void remove_comment(char *token);
+void handleExit(char **tokens, char *buffer);
 void make_array(char **array, char *token, int *i);
-void execution(char *fullcommand, char **array, char **envp, int *i, int *last_command_status, char **argv);
+void execution(char *fullcommand, char **array, char **envp, int *i);
 void create_full_command(char **fullcommand, const char *singlepath, const char *array0);
 void cleanup(char *fullcommand, char **array);
 int count_env(char **environ);
 void modify_environment_entry(char **new_environ, int count, const char *name, const char *value);
-void handleExit(char **tokens, char *buffer, int *last_command_status);
-void handle_special_commands(char **array, int *last_command_status);
 
 /* Variables */
 char extern **environ;
